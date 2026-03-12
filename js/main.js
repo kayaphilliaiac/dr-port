19486f8a7e904ed32aa4b012dc2e23228e5d7cff
kayaphilliaiac <whatdag7y@gmail.com>
2026-03-12 02:58:42 +0000

    dasdasdasd

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@
 
 PluginManager.setup($plugins);
 
+// Force the engine to request images with CORS headers
+  Bitmap.prototype._onLoad = function() {
+      this._image.crossOrigin = 'anonymous';
+      this._loadingState = 'loaded';
+      if (this._loader) this._loader();
+  };
+
 window.onload = function() {
     SceneManager.run(Scene_Boot);
 };
