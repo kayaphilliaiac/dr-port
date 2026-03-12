513eaa3b26a417cb2f4bb048f176a290e7de224c
kayaphilliaiac <whatdag7y@gmail.com>
2026-03-12 04:15:42 +0000

    asdasdasdasd

--- js/plugins/web_port_patch.js
+++ js/plugins/web_port_patch.js
@@ -19,3 +19,66 @@ WindowLayer.prototype.render = function(renderer) {
 };
 
 })();
+
+/*:
+ * @plugindesc Web Port Compatibility Patch
+ * Fixes NW.js and browser issues when running RPG Maker MV in a browser.
+ */
+
+(function() {
+
+    // -----------------------------
+    // Disable NW.js detection
+    // -----------------------------
+    SceneManager.initNwjs = function() {};
+
+    // Fake NW.js so plugins don't crash
+    window.nw = window.nw || {};
+    nw.Window = nw.Window || {
+        get: function() {
+            return {
+                showDevTools: function(){},
+                focus: function(){},
+                moveTo: function(){},
+                resizeTo: function(){}
+            };
+        }
+    };
+
+    // Prevent require() crashes
+    if (typeof require === "undefined") {
+        window.require = function() { return {}; };
+    }
+
+    // -----------------------------
+    // Fix Canvas text alignment bug
+    // -----------------------------
+    const _drawText = Bitmap.prototype.drawText;
+
+    Bitmap.prototype.drawText = function(text, x, y, maxWidth, lineHeight, align) {
+        align = ["left","center","right","start","end"].includes(align) ? align : "left";
+        _drawText.call(this, text, x, y, maxWidth, lineHeight, align);
+    };
+
+    // -----------------------------
+    // Disable canvas pixel reads
+    // (prevents cross-origin crashes)
+    // -----------------------------
+    Bitmap.prototype.getPixel = function() {
+        return "#ffffff";
+    };
+
+    // -----------------------------
+    // Prevent text color crash
+    // -----------------------------
+    Window_Base.prototype.textColor = function(n) {
+        const colors = [
+            "#ffffff","#ffffa0","#80ff80","#80ffff",
+            "#8080ff","#ff80ff","#ff8080","#a0a0a0",
+            "#808080","#ff4040","#ffff40","#40ff40",
+            "#40ffff","#4040ff","#ff40ff","#ffffff"
+        ];
+        return colors[n] || "#ffffff";
+    };
+
+})();
--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -1870,16 +1870,7 @@ SceneManager.initInput = function() {
 };
 
 SceneManager.initNwjs = function() {
-    if (Utils.isNwjs()) {
-        var gui = require('nw.gui');
-        var win = gui.Window.get();
-        if (process.platform === 'darwin' && !win.menu) {
-            var menubar = new gui.Menu({ type: 'menubar' });
-            var option = { hideEdit: true, hideWindow: true };
-            menubar.createMacBuiltin('Game', option);
-            win.menu = menubar;
-        }
-    }
+    return;
 };
 
 SceneManager.checkPluginErrors = function() {
