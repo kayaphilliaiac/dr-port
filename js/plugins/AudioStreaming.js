cb640756a8452dcb5262eeb6433d7f00da6e9a7c
kayaphilliaiac <whatdag7y@gmail.com>
2026-04-30 21:11:05 +0000

    reenable saba

--- js/plugins/AudioStreaming.js
+++ js/plugins/AudioStreaming.js
@@ -193,7 +193,7 @@ AudioManager.audioFileExt = function() {
     return '.ogg';
 };
 
-fetch('').catch(_ => window.cordova = window.cordova || true);
+window.cordova = window.cordova || true;
 
 if (window.ResourceHandler) {
     ResourceHandler.fetchWithRetry = async function(
